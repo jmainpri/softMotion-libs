68225fefa0fd1e829fcd294c9cd157f6bb32e1a8
xwang <xwang@laas.fr>
2010-07-06 18:12:39 +0200

    add comments

--- qt/mainwindow.h
+++ qt/mainwindow.h
@@ -7,6 +7,35 @@
 #include "ui_mainwindow.h"
 #include "gbM/gbGENOM.h"
 
+
+
+/** @mainpage QSoftMotionPlanner 
+ *
+ * @section Copyright 
+ *
+ * Copyright (c) 2010 LAAS/CNRS
+ * All rights reserved.
+ * 
+ * Permission to use, copy, modify, and distribute this software for any purpose
+ * with or without   fee is hereby granted, provided   that the above  copyright
+ * notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ *
+ *                                           Xavier BROQUERE on Feb 01 2010
+ *
+ * @section Introduction
+ *
+ * Software description here
+ */ 
+
+
 /** @file mainwindow.h
  * @brief Declaration of the MainWindow class
  */
@@ -35,6 +64,17 @@ public:
      * The destructor of class MainWindow
      */
     ~MainWindow();
+    
+     /** @brief setDisplay
+     *  
+     *  only at startup of the program , if display == flase
+     *  the Qt interface is not launched
+     *
+     * @param val : bool value
+     */    
+    void setDisplay(bool val) {
+      _display = val;
+    }
     
     /** @brief load the file
      *  
@@ -196,6 +236,7 @@ private:
     int _nbCurve;
     std::string _fileName;
     bool _isFullScreen;
+    bool _display;
 
     
 };
